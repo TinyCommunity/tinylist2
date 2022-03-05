cecd1bd2645fc0ea8ee3dc5c2f9a704692236922
Calvin Tam <calvintam236@users.noreply.github.com>
2022-03-05 00:50:13 -0800

    feat: add .length, .indexOf(), .contains() tests

--- test/run.dart
+++ test/run.dart
@@ -4,119 +4,293 @@ import 'package:trotter/trotter.dart';
 void main() {
   final bagOfItems = characters('abcde');
 
-  test(
-    "Combinations generates a pseudo-list",
+  group(
+    "Combinations",
     () {
       final combos = Combinations(3, bagOfItems);
+      /*
       for (final combo in combos()) {
         print('$combo (${combos.indexOf(combo)})');
       }
-      expect(
-        combos.indexOf(["a", "c", "e"]),
-        BigInt.from(4),
+      */
+      test(
+        ".length",
+        () {
+          expect(
+            combos.length,
+            BigInt.from(10),
+          );
+        },
       );
-      expect(
-        combos.indexOf(["e", "c", "a"]),
-        BigInt.from(4),
+      test(
+        ".indexOf()",
+        () {
+          expect(
+            combos.indexOf(["a", "c", "e"]),
+            BigInt.from(4),
+          );
+          expect(
+            combos.indexOf(["e", "c", "a"]),
+            BigInt.from(4),
+          );
+        },
       );
-    }
+      test(
+        ".contains()",
+        () {
+          expect(
+            combos.contains(["a", "c", "e"]),
+            true,
+          );
+          expect(
+            combos.contains(["a", "e", "g"]),
+            false,
+          );
+        },
+      );
+    },
   );
 
-  test(
-    "Permutations generates a pseudo-list",
+  group(
+    "Permutations",
     () {
       final perms = Permutations(3, bagOfItems);
+      /*
       for (final perm in perms()) {
         print('$perm (${perms.indexOf(perm)})');
       }
-      expect(
-        perms.indexOf(["a", "c", "e"]),
-        BigInt.from(24),
+      */
+      test(
+        ".length",
+        () {
+          expect(
+            perms.length,
+            BigInt.from(60),
+          );
+        },
       );
-      expect(
-        perms.indexOf(["e", "c", "a"]),
-        BigInt.from(27),
+      test(
+        ".indexOf()",
+        () {
+          expect(
+            perms.indexOf(["a", "c", "e"]),
+            BigInt.from(24),
+          );
+          expect(
+            perms.indexOf(["e", "c", "a"]),
+            BigInt.from(27),
+          );
+        },
       );
-    }
+      test(
+        ".contains()",
+        () {
+          expect(
+            perms.contains(["a", "c", "e"]),
+            true,
+          );
+          expect(
+            perms.contains(["a", "e", "g"]),
+            false,
+          );
+        },
+      );
+    },
   );
 
-  test(
-    "Compositions generates a pseudo-list",
+  group(
+    "Compositions",
     () {
       final compos = Compositions(3, bagOfItems);
+      /*
       for (final compo in compos()) {
         print('$compo (${compos.indexOf(compo)})');
       }
-      expect(
-        compos.indexOf(["a", "c", "e"]),
-        BigInt.from(11),
+      */
+      test(
+        ".length",
+        () {
+          expect(
+            compos.length,
+            BigInt.from(35),
+          );
+        },
       );
-      expect(
-        compos.indexOf(["e", "c", "a"]),
-        BigInt.from(11),
+      test(
+        ".indexOf()",
+        () {
+          expect(
+            compos.indexOf(["a", "c", "e"]),
+            BigInt.from(11),
+          );
+          expect(
+            compos.indexOf(["e", "c", "a"]),
+            BigInt.from(11),
+          );
+        },
       );
-    }
+      test(
+        ".contains()",
+        () {
+          expect(
+            compos.contains(["a", "c", "e"]),
+            true,
+          );
+          expect(
+            compos.contains(["a", "e", "g"]),
+            false,
+          );
+        },
+      );
+    },
   );
 
-  test(
-    "Amalgams generates a pseudo-list",
+  group(
+    "Amalgams",
     () {
       final amals = Amalgams(3, bagOfItems);
+      /*
       for (final amal in amals()) {
         print('$amal (${amals.indexOf(amal)})');
       }
-      expect(
-        amals.indexOf(["a", "c", "e"]),
-        BigInt.from(14),
+      */
+      test(
+        ".length",
+        () {
+          expect(
+            amals.length,
+            BigInt.from(125),
+          );
+        },
       );
-      expect(
-        amals.indexOf(["e", "c", "a"]),
-        BigInt.from(110),
+      test(
+        ".indexOf()",
+        () {
+          expect(
+            amals.indexOf(["a", "c", "e"]),
+            BigInt.from(14),
+          );
+          expect(
+            amals.indexOf(["e", "c", "a"]),
+            BigInt.from(110),
+          );
+        },
       );
-    }
+      test(
+        ".contains()",
+        () {
+          expect(
+            amals.contains(["a", "c", "e"]),
+            true,
+          );
+          expect(
+            amals.contains(["a", "e", "g"]),
+            false,
+          );
+        },
+      );
+    },
   );
 
-  test(
-    "Subsets generates a pseudo-list",
+  group(
+    "Subsets",
     () {
       final subs = Subsets(bagOfItems);
+      /*
       for (final sub in subs()) {
         print('$sub (${subs.indexOf(sub)})');
       }
-      expect(
-        subs.indexOf(["c"]),
-        BigInt.from(4),
+      */
+      test(
+        ".length",
+        () {
+          expect(
+            subs.length,
+            BigInt.from(32),
+          );
+        },
       );
-      expect(
-        subs.indexOf(["a", "c", "e"]),
-        BigInt.from(21),
+      test(
+        ".indexOf()",
+        () {
+          expect(
+            subs.indexOf(["c"]),
+            BigInt.from(4),
+          );
+          expect(
+            subs.indexOf(["a", "c", "e"]),
+            BigInt.from(21),
+          );
+          expect(
+            subs.indexOf(["a", "b", "c", "d", "e"]),
+            BigInt.from(31),
+          );
+        },
       );
-      expect(
-        subs.indexOf(["a", "b", "c", "d", "e"]),
-        BigInt.from(31),
+      test(
+        ".contains()",
+        () {
+          expect(
+            subs.contains(["a", "c", "e"]),
+            true,
+          );
+          expect(
+            subs.contains(["a", "e", "g"]),
+            false,
+          );
+        },
       );
-    }
+    },
   );
 
-  test(
-    "Compounds generates a pseudo-list",
+  group(
+    "Compounds",
     () {
       final comps = Compounds(bagOfItems);
+      /*
       for (final comp in comps()) {
         print('$comp (${comps.indexOf(comp)})');
       }
-      expect(
-        comps.indexOf(["c"]),
-        BigInt.from(3),
+      */
+      test(
+        ".length",
+        () {
+          expect(
+            comps.length,
+            BigInt.from(326),
+          );
+        },
       );
-      expect(
-        comps.indexOf(["a", "c", "e"]),
-        BigInt.from(50),
+      test(
+        ".indexOf()",
+        () {
+          expect(
+            comps.indexOf(["c"]),
+            BigInt.from(3),
+          );
+          expect(
+            comps.indexOf(["a", "c", "e"]),
+            BigInt.from(50),
+          );
+          expect(
+            comps.indexOf(["a", "b", "c", "d", "e"]),
+            BigInt.from(206),
+          );
+        },
       );
-      expect(
-        comps.indexOf(["a", "b", "c", "d", "e"]),
-        BigInt.from(206),
+      test(
+        ".contains()",
+        () {
+          expect(
+            comps.contains(["a", "c", "e"]),
+            true,
+          );
+          expect(
+            comps.contains(["a", "e", "g"]),
+            false,
+          );
+        },
       );
-    }
+    },
   );
 }
